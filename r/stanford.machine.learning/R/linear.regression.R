4054f0f096f64e49ed3d30e28e938bb7d53efc08
Shane Conway <shane.conway@gmail.com>
2011-10-22 14:33:05 -0400

    Updated package with demo for univariate linear regression

--- r/stanford.machine.learning/R/linear.regression.R
+++ r/stanford.machine.learning/R/linear.regression.R
@@ -1,3 +1,25 @@
 
 # Create the loss function
 loss <- function(intercept, slope, x, y) sum(((intercept + (slope * x)) - y)^2)/2
+
+
+# Gradient descent function
+grad <- function(x, y, theta) {
+  gradient <- (t(x) %*% ((x %*% t(theta)) - y))
+  return(t(gradient))
+}
+
+# Show the path of a gradient descent parameter optimization
+gradient.path <- function(x) {
+    # Initialize the parameters
+    theta <- matrix(c(0, 0), nrow=1)
+
+    # Look at the values over each iteration
+    theta.path <- matrix(ncol=2)
+    for (i in 1:500) {
+      theta <- theta - alpha * 1/m * grad(x, y, theta)
+	if(all(is.na(theta))) break
+      theta.path <- rbind(theta.path, theta)
+    }
+    theta.path
+}
